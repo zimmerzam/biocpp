12acfca2b0e802fabaf1247cb7ad9117af7952dd
zimmer <zimmerzam@gmail.com>
2013-04-10 17:44:18 +0200

    fasta now accept holes -

--- src/pdb/pdb_SEQRES_format.h
+++ src/pdb/pdb_SEQRES_format.h
@@ -131,7 +131,7 @@ pdb_seqres_record read_pdb_seqres_record( char buffer[] ){
 		">chain_id_1", i.e. ">A"
 */
 pdb_seqres_record read_fasta(std::string& fasta){
-  fasta.erase(std::remove_if(fasta.begin(), fasta.end(), [](char ch){if( std::isalpha(ch) or ch=='>'  ) return false; return true;}), fasta.end());
+  fasta.erase(std::remove_if(fasta.begin(), fasta.end(), [](char ch){if( std::isalpha(ch) or ch=='>' or ch=='-'  ) return false; return true;}), fasta.end());
 	pdb_seqres_record record;
 	unsigned int i = 0;
 	while(fasta[i]!='>' or not std::isalpha(fasta[i+1]) ){
