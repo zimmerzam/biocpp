9d47b051e58c8e4abc3e2d6fb7cff7af7969bb25
zimmerzam <zimmerzam@gmail.com>
2013-07-10 12:21:08 +0200

    temporary change in pdb.h: print line with unknown atom name

--- examples/test.cpp
+++ examples/test.cpp
@@ -1,6 +1,9 @@
 #include <BioCpp.h>
-#include <iostream>
 
-int main(){
+int main( int argc, char* argv[] ){
+  for(int i = 1; i < argc; ++i){
+    BioCpp::pdb PDB(argv[i], 0);
+  }
+
   return 0;
 }
