aeabf95331bbcf2fa4826219538a855d18c37327
zimmerzam <zimmerzam@gmail.com>
2014-01-15 16:14:15 +0100

    backbone oxygen reconstruction

---
+++ src/BioCpp/proteins/atom_position_reconstruction/proteinGeometry.hpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*    Copyright 2013 Stefano Zamuner                                          */
+/*                                                                            */
+/*    This file is part of BioCpp.                                            */
+/*                                                                            */
+/*    BioCpp is free software: you can redistribute it and/or modify          */
+/*    it under the terms of the GNU General Public License as published by    */
+/*    the Free Software Foundation, either version 3 of the License, or       */
+/*    (at your option) any later version.                                     */
+/*                                                                            */
+/*    BioCpp is distributed in the hope that it will be useful,               */
+/*    but WITHOUT ANY WARRANTY; without even the implied warranty of          */
+/*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           */
+/*    GNU General Public License for more details.                            */
+/*                                                                            */
+/*    You should have received a copy of the GNU General Public License       */
+/*    along with BioCpp.  If not, see <http://www.gnu.org/licenses/>.         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PROTEIN_GEOMETRY_PARAMETERS_H
+#define PROTEIN_GEOMETRY_PARAMETERS_H
+
+namespace BioCpp{
+namespace protein{
+
+const double N_H_bond_length = 1.0;
+const double C_O_bond_length = 1.0;
+const double CA_CB_bond_length = 1.0;
+
+const double cos_N_CA_C_angle = ;
+const double cos_N_CA_CB_angle = ;
+const double cos_CA_C_N_angle = ;
+const double cos_CA_C_O_angle = ;
+const double cos_C_N_CA_angle = ;
+const double cos_C_N_H_angle = ;
+
+} // end namespace
+}
+#endif
